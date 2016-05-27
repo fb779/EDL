545d88a8dccd3056a1f8d585ef796e81046e1ff0
nfforeror <nfforeror@dane.gov.co>
2016-05-27 15:01:59 -0500

    Se crea el proyecto con codeigniter y se configura para manejo de arquitectura HMVC

---
+++ application/modules/home/controllers/Home.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+//header ('Content-type: text/html; charset=utf-8');
+
+class Home extends MX_Controller {
+	
+	public function __construct() {
+		parent::__construct();
+
+	}
+	
+	public function index()
+	{
+		$datos['dt'] = array(
+			'static' => BASE_STATICS, 
+			'title' => TITULO_ENCUESTA,
+			'navigate' => array(	
+				array('modulo' => 'Directorio1', 'direc' => 'empresas'),
+				//array('modulo' => 'Usuarios', 'direc' => 'usuarios'),
+			),
+		);
+		
+		$datos['page'] = 'prueba';
+		$datos['dt_page'] = array(
+			'url_login' => BASE_URL . 'login',
+			'parrafo' => 'Parrafo de verificacion para la carga de contenido de la pagina ',
+		);
+
+		$this->parser->parse('loadTemplates', $datos, TRUE);
+	}
+}
